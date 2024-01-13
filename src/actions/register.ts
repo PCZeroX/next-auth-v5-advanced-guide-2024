e85548ca517eb6b65512890ac441051416f85ba2
pczerox <pczeroxstore@gmail.com>
2024-01-13 01:07:59 -0500

    Database and Prisma setup - Create user

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,12 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+
 import { RegisterSchema } from "@/schemas";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +15,32 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  // const existingUser = await db.user.findUnique({
+  //   where: {
+  //     email,
+  //   },
+  // });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // T O D O : Send verification token email
+
+  // const verificationToken = await generateVerificationToken(email);
+  // await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+  return { success: "Confirmation email sent!" };
 };
